7e95d50b3f63707fdc5f4a6296048cffb5d94d09
branyoe <bhernandez1@ucol.mx>
2024-04-29 22:22:38 -0600

    feat: add "vista inicial" view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main>
-      Welcome to MiUDC
-    </main>
+    <div className="h-screen flex flex-col justify-center align-middle p-4">
+      <div className="flex justify-center">
+        <Image
+          src="/logo.svg"
+          alt="MiUDC logo"
+          width={120}
+          height={120}
+        />
+      </div>
+      <div className="mb-6 text-center text-lg font-semibold">
+        Creando puentes de conexión entre egresados y aspirantes.
+      </div>
+      <div className="w-full flex justify-center text-white">
+        <Link
+          className="bg-primary w-full font-extrabold p-3 rounded-full text-center"
+          href="/"
+        >
+          Continuar
+        </Link>
+      </div>
+    </div>
   );
 }
