eb960e3e29e8ca42b485d9ef517efbf9c3c7a3a1
eduardoChacom <lchacon0@ucol.mx>
2024-06-05 12:40:30 -0600

    feat: add sign-in view

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -3,17 +3,19 @@ import Link from "next/link";
 interface props {
   text: string;
   path: string;
-  variant: "green" | "smoothGreen" | "link";
+  variant: "green" | "smoothGreen" | "link" | "forgetGreen";
 }
 
 export default function Button({ text, path, variant }: props) {
   return (
-    <div className="flex justify-center w-full py-2">
+    <div className={`flex ${variant === "forgetGreen" ? "justify-start" : "justify-center"} w-full py-2`}>
       <Link
-        className={`w-full text-center font-semibold py-3 rounded-full ${
+        className={`font-semibold py-3 rounded-full ${
           variant === "link"
-            ? "text-gray-700 text-lg"
-            : `bg-${variant} text-white`
+            ? "text-gray-700 text-lg w-full text-center"
+            : variant === "forgetGreen"
+            ? "underline text-green text-sm text-left"
+            : `bg-${variant} text-white w-full text-center`
         }`}
         href={path}
       >
