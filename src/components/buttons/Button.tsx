a5877ee1b4afd83a8bb694e5d03eef54ca4fa26e
Luis Pinto <lpinto1@ucol.mx>
2024-06-11 01:57:37 -0600

    refactor: use object bracket notation to handle variants and flex in Button component

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -7,31 +7,24 @@ interface Props {
 }
 
 export default function Button({ text, path, variant }: Props) {
-  let variantClasses;
+  const variantClasses = {
+    link: "text-gray-700 text-lg w-full text-center",
+    forgetGreen: "underline text-green text-sm text-left",
+    green: `bg-${variant} text-white w-full text-center shadow-md rounded-full`,
+    smoothGreen: `bg-${variant} text-green w-full text-center shadow-md rounded-full`,
+  }[variant];
 
-  switch (variant) {
-    case "link":
-      variantClasses = "text-gray-700 text-lg w-full text-center";
-      break;
-    case "forgetGreen":
-      variantClasses = "underline text-green text-sm text-left";
-      break;
-    case "green":
-      variantClasses = `bg-${variant} text-white w-full text-center shadow-md rounded-full`;
-      break;
-    case "smoothGreen":
-      variantClasses = `bg-${variant} text-green w-full text-center shadow-md rounded-full`;
-      break;
-    default:
-      variantClasses = "";
-  }
+  const flexClasses =
+    {
+      forgetGreen: "justify-start",
+      default: "justify-center",
+      green: "justify-center",
+      link: "justify-center",
+      smoothGreen: "justify-center",
+    }[variant] || "justify-center";
 
   return (
-    <div
-      className={`flex ${
-        variant === "forgetGreen" ? "justify-start" : "justify-center"
-      } w-full py-2`}
-    >
+    <div className={`flex ${flexClasses} w-full py-2`}>
       <Link
         className={`font-semibold py-3 rounded-full ${variantClasses}`}
         href={path}
