07759682852634365584bd1abc5caeb6f5b17981
Luis Pinto <lpinto1@ucol.mx>
2024-06-17 01:52:10 -0600

    chore: add xs screen size

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+        xs: "300px",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
