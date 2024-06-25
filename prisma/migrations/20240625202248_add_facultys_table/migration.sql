d3e227a51497600f148d6a977cc76ed176c67079
branyoe <bhernandez1@ucol.mx>
2024-06-25 14:23:27 -0600

    feat: add Faculty model to prisma schema

---
+++ prisma/migrations/20240625202248_add_facultys_table/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Faculty" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Faculty_pkey" PRIMARY KEY ("id")
+);
