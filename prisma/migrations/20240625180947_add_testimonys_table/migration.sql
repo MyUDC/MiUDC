278fee77b40850dca5f0ab21ab274fc36b3a1f2c
branyoe <bhernandez1@ucol.mx>
2024-06-25 14:31:42 -0600

    refactor: Remove database tables and migration files

--- prisma/migrations/20240625180947_add_testimonys_table/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Testimony" (
-    "id" TEXT NOT NULL,
-    "title" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Testimony_pkey" PRIMARY KEY ("id")
-);
