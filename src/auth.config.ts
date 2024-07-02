0f2fee7e7d77be8395107b0eee2a75b35bbe7484
branyoe <bhernandez1@ucol.mx>
2024-07-02 14:00:51 -0600

    chore: add credentials provider to auth config

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,9 +1,30 @@
 import type { NextAuthConfig } from 'next-auth';
- 
+import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
+
+
 export const authConfig: NextAuthConfig = {
   pages: {
     signIn: '/sign-in',
     newUser: '/sign-up/register',
   },
-  providers: []
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+        
+        if (!parsedCredentials.success) return null;
+        
+        const { email, password } = parsedCredentials.data;
+        console.log({ email, password });
+
+        return null;
+      },
+    }),
+  ]
 };
+
+export const { signIn, signOut, auth } = NextAuth(authConfig);
