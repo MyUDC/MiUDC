de6fa46dc6e121a343193c50e854a2b4c5c194d1
branyoe <bhernandez1@ucol.mx>
2024-07-23 12:13:28 -0600

    perf: enable font preload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  preload: true
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
