45df89d3847f639064a94204073ff06ea05c91f4
branyoe <bhernandez1@ucol.mx>
2024-08-15 10:12:27 -0600

    chore: add fix note

--- src/features/auth/components/SignOutButton.tsx
+++ src/features/auth/components/SignOutButton.tsx
@@ -3,18 +3,16 @@
 import { faChevronRight, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useRouter } from 'next/navigation';
-import { SignOut } from '../actions';
+import { signOut } from 'next-auth/react';
 
 export const SignOutButton = () => {
   const router = useRouter();
-  
-  
+
   return (
-    <div onClick={() => {
-      console.log(process.env.NODE_ENV);
-      SignOut();
-      router.refresh();
-    }} className="cursor-pointer mb-5 w-full">
+    <div
+      // todo: fix sign-out function
+      onClick={() => signOut()}
+      className="cursor-pointer mb-5 w-full">
       <div className="flex justify-between items-center px-4 mb-4">
         <div className="flex items-center">
           <FontAwesomeIcon
