cfb8402c4795cf72b56cf2d0864353740b58fc92
Luis Pinto <lpinto1@ucol.mx>
2024-08-15 19:48:03 -0600

    chore: implementation of ImageView in CareerImage

--- src/app/(auth)/(no-app-bar))/career-forum/page.tsx
+++ src/app/(auth)/(no-app-bar))/career-forum/page.tsx
@@ -1,23 +1,41 @@
 "use client";
+import { useState } from "react";
 import CareerImage from "@/features/career-forum/components/CareerImage/CareerImage";
 import AddButton from "@/shared/components/AddButton";
 import CareerContent from "@/features/career-forum/CareerContent";
+import ImageView from "@/shared/components/ImageView";
 
 export default function CareerForumView() {
-  return (
-    <div className="max-w-2xl mx-auto p-4">
-      <div className="border border-gray-300 rounded-lg p-6 md:p-12">
-        <CareerImage src="/telematica.jpg" />
-        <div className="mt-4 mb-4 text-left text-xl font-semibold text-gray-800">
-          Ingeniería de Software
-          <div className="text-green font-semibold text-base">
-            Facultad de Telemática
-          </div>
-        </div>
+  const [showImageView, setShowImageView] = useState(false);
 
-        <CareerContent />
-        <AddButton />
+  return (
+    <div className="max-w-2xl mx-auto p-2">
+      <div className="border border-gray-300 rounded-lg">
+        <CareerImage
+          src="/telematica.jpg"
+          onClick={() => setShowImageView(true)}
+          onClose={() => setShowImageView(false)} // Maneja el cierre
+        />
+        <div className="px-6 md:px-12">
+          <div className="mt-4 mb-4 text-left text-xl font-semibold text-gray-800">
+            Ingeniería de Software
+            <div className="text-green font-semibold text-base">
+              Facultad de Telemática
+            </div>
+          </div>
+
+          <CareerContent />
+          <AddButton />
+        </div>
       </div>
+
+      {showImageView && (
+        <ImageView
+          imageUrls={["/telematica.jpg"]}
+          initialIndex={0}
+          onClose={() => setShowImageView(false)}
+        />
+      )}
     </div>
   );
 }
--- src/features/career-forum/components/CareerImage/CareerImage.tsx
+++ src/features/career-forum/components/CareerImage/CareerImage.tsx
@@ -1,19 +1,56 @@
 import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faEllipsisH } from "@fortawesome/free-solid-svg-icons";
 
 type CareerImageProps = {
   src: string;
+  onClick?: () => void;
+  onClose?: () => void; // Añadir una propiedad para el cierre
 };
 
-export default function CareerImage({ src }: CareerImageProps) {
+export default function CareerImage({
+  src,
+  onClick,
+  onClose,
+}: CareerImageProps) {
   return (
-    <div className="relative w-full h-48 rounded-lg overflow-hidden">
+    <div
+      className="relative w-full h-48 overflow-hidden rounded-t-lg cursor-pointer select-none"
+      onClick={onClick}
+    >
       <Image
         src={src}
         alt="Imagen de la Carrera"
         objectFit="cover"
-        className="max-w-full max-h-full object-contain"
+        className="max-w-full max-h-full object-cover"
         layout="fill"
+        draggable="false"
       />
+
+      {/* Botón de cierre */}
+      <button
+        onClick={(e) => {
+          e.stopPropagation(); // Evita que el clic cierre el contenedor
+          if (onClose) onClose(); // Llama a la función de cierre si está definida
+        }}
+        className="absolute top-4 left-4 bg-gray-800 bg-opacity-70 rounded-full p-2 px-3 hover:opacity-75 z-10"
+        title="Cerrar"
+      >
+        <FontAwesomeIcon icon={faTimes} className="text-white text-xl" />
+      </button>
+
+      {/* Botón de opciones */}
+      <button
+        onClick={(e) => {
+          e.stopPropagation(); // Evita que el clic cierre el contenedor
+          if (onClose) onClose(); // Llama a la función de cierre si está definida
+        }}
+        className="absolute top-4 right-4 bg-gray-800 bg-opacity-70 rounded-full p-2 px-3 hover:opacity-75 z-10"
+        title="Opciones"
+      >
+        <FontAwesomeIcon icon={faEllipsisH} className="text-white text-xl" />
+      </button>
     </div>
   );
 }
