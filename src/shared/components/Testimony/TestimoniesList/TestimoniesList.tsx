1ab4e14a1b060e6ac99e091d05bd8433cdd9574d
branyoe <bhernandez1@ucol.mx>
2024-08-17 08:06:57 -0600

    feat: add loading component to testimoniesList

--- src/shared/components/Testimony/TestimoniesList/TestimoniesList.tsx
+++ src/shared/components/Testimony/TestimoniesList/TestimoniesList.tsx
@@ -5,7 +5,8 @@ import { type Testimony } from "@prisma/client";
 import InfiniteScroll from "react-infinite-scroll-component";
 
 import paginateTestimony from "@/shared/actions/paginateTestimonies";
-import { Loader } from "./Loader";
+import { Loading } from "./Loading";
+import TestimonyComponent from "../Testimony";
 
 interface Props {
   initTestimonies: Testimony[];
@@ -16,22 +17,26 @@ export const TestimoniesList = ({ initTestimonies }: Props) => {
   const [testimonies, setTestimonies] = useState(initTestimonies)
 
   return (
-    <InfiniteScroll
-      hasMore
-      next={() => {
-        setTimeout(async () => {
-          const newTestimonies = await paginateTestimony(3, testimonies.length)
-          setTestimonies(testimonies.concat(...newTestimonies));
-        }, 3000);
-      }}
-      loader={<Loader/>}
-      dataLength={testimonies.length}
-    >
-      {
-        testimonies.map((testimony: Testimony) => (
-          <div className="h-[500px] m-2 bg-slate-400" key={testimony.id}>{testimony.title}</div>
-        ))
-      }
-    </InfiniteScroll>
+    <div className="flex flex-col items-center">
+      <InfiniteScroll
+        hasMore
+        next={() => {
+          setTimeout(async () => {
+            const newTestimonies = await paginateTestimony(3, testimonies.length)
+            setTestimonies(testimonies.concat(...newTestimonies));
+          }, 3000);
+        }}
+        loader={<Loading />}
+        dataLength={testimonies.length}
+      >
+        {testimonies.map((testimony: Testimony) => (
+          <TestimonyComponent
+            key={testimony.id}
+            createdAt={testimony.createdAt}
+            content={testimony.content}
+          />
+        ))}
+      </InfiniteScroll>
+    </div>
   )
 }
