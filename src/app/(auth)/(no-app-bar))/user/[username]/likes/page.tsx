599e5099c8fcae0d12840c282e8706a8e818ae79
Luis Pinto <lpinto1@ucol.mx>
2024-09-05 06:39:23 -0600

    chore: add liked posts page

--- src/app/(auth)/(no-app-bar))/user/[username]/likes/page.tsx
+++ src/app/(auth)/(no-app-bar))/user/[username]/likes/page.tsx
@@ -1,3 +1,45 @@
-export default function Likes() {
-  return <h1>Likes</h1>;
+// src/pages/UserLikePage.tsx
+import getUserByUsername from "@/features/auth/actions/getUserByUsername";
+import paginateLikedPostsByUser from "@/shared/actions/Post/paginateLikedPostByUser";
+import { PostList } from "@/shared/components/Testimony/PostList/PostList";
+import { PostType } from "@prisma/client";
+
+interface Props {
+  params: {
+    username: string;
+  };
+}
+
+export default async function UserLikePage({ params }: Props) {
+  const { username } = params;
+  const user = await getUserByUsername(username);
+
+  // Definimos los tipos de publicaciones que queremos recuperar
+  const postTypes = [PostType.QUESTION, PostType.TESTIMONY, PostType.REPLY];
+
+  const initLikedPosts = await paginateLikedPostsByUser(
+    4,
+    0,
+    user!.id,
+    postTypes
+  );
+
+  return (
+    <div className="flex justify-center">
+      <div className="w-svw sm:max-w-lg">
+        <PostList
+          initPosts={initLikedPosts}
+          paginateHandler={async (take: number, skip: number) => {
+            "use server";
+            return await paginateLikedPostsByUser(
+              take,
+              skip,
+              user!.id,
+              postTypes
+            );
+          }}
+        />
+      </div>
+    </div>
+  );
 }
