17019670866a2f7a8a4430223e9481a3c38fce21
Luis Pinto <lpinto1@ucol.mx>
2024-09-10 01:12:04 -0600

    chore: separate ProfileForm component of UserProfileEditor.tsx

--- src/shared/components/UserProfileEditor.tsx
+++ src/shared/components/UserProfileEditor.tsx
@@ -1,9 +1,7 @@
 "use client";
-import * as React from "react";
 import { useState } from "react";
 import { useMediaQuery } from "@react-hook/media-query";
 
-import { cn } from "@/lib/utils";
 import {
   Drawer,
   DrawerClose,
@@ -22,9 +20,9 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
+
+import ProfileForm from "./ProfileForm";
 
 export default function UserProfileEditor() {
   const [open, setOpen] = useState(false);
@@ -82,41 +80,5 @@ export default function UserProfileEditor() {
         </DrawerFooter>
       </DrawerContent>
     </Drawer>
-  );
-}
-
-function ProfileForm({ className }: React.ComponentProps<"form">) {
-  return (
-    <form className={cn("grid items-start gap-4", className)}>
-      <div className="grid gap-2">
-        {/** agregar el cambiar foto */}
-        <Label htmlFor="name">Nombre de usuario</Label>
-        <Input
-          type="text"
-          id="name"
-          placeholder="Escribe tu nombre"
-          defaultValue="John Doe"
-        />
-      </div>
-      <div className="grid gap-2">
-        <Label htmlFor="biography">Biografía</Label>
-        <Input
-          type="text"
-          id="biography"
-          maxLength={160}
-          placeholder="Escribe tu biografía"
-        />
-        <span className="text-sm text-gray-500">Máximo 160 caracteres</span>
-      </div>
-      <div className="grid gap-2">
-        <Label htmlFor="major">Carrera Universitaria</Label>
-        {/** cambiar por un select de shadcn*/}
-        <Input type="text" id="major" defaultValue="Ingeniería en Sistemas" />
-      </div>
-      <div className="grid gap-2">
-        <Label htmlFor="semester">Semestre</Label>
-        <Input type="number" id="semester" defaultValue={3} min={1} max={10} />
-      </div>
-    </form>
   );
 }
