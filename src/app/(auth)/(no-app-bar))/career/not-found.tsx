36685dd351f85523b5f28dd704c235d386296320
Luis Pinto <lpinto1@ucol.mx>
2024-09-10 19:23:05 -0600

    refactor: delete app-bar and no-app-bar files

--- src/app/(auth)/(no-app-bar))/career/not-found.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function NotFoundPageBySlug() {
-  return (
-    <div>
-      <h1 className="h-1">404</h1>
-    </div>
-  );
-}
--- src/app/(auth)/(no-app-bar))/user/not-found.tsx
+++
@@ -1,7 +0,0 @@
-export default function userNotFound() {
-  return (
-    <div>
-      <h1 className="h-1">404</h1>
-    </div>
-  );
-}
