f36a22d92c3c86113479c5c6a93bf1f210183249
Luis Pinto <lpinto1@ucol.mx>
2024-09-15 20:58:31 -0600

    chore: add NewPost component on layout of forum

--- src/app/(auth)/(no-bottom-nav)/career/[careerSlug]/(layout)/forum/layout.tsx
+++ src/app/(auth)/(no-bottom-nav)/career/[careerSlug]/(layout)/forum/layout.tsx
@@ -1,10 +1,12 @@
 import { ServerTabs } from "@/features/career/components/ServerTabs";
+import NewPost from "@/shared/components/PostForm/NewPostDrawer";
+import { Toaster } from "@/components/ui/toaster";
 
 interface Props {
   children: React.ReactNode;
   params: {
     careerSlug: string;
-  }
+  };
 }
 
 export default async function CareerForumLayout({ children, params }: Props) {
@@ -12,23 +14,25 @@ export default async function CareerForumLayout({ children, params }: Props) {
 
   const tabs = [
     {
-      text: 'Testimonios',
-      path: `/career/${careerSlug}/forum/testimonies`
+      text: "Testimonios",
+      path: `/career/${careerSlug}/forum/testimonies`,
     },
     {
-      text: 'Preguntas',
-      path: `/career/${careerSlug}/forum/questions`
+      text: "Preguntas",
+      path: `/career/${careerSlug}/forum/questions`,
     },
-  ]
+  ];
 
   return (
-    <div>
-      <div className="sticky z-50 top-0">
+    <div className="flex flex-col min-h-screen">
+      <div className="sticky z-40 top-0 border-b bg-background">
         <ServerTabs tabs={tabs} />
       </div>
-      <div>
-        {children}
+      <div className="flex-grow">
+        <NewPost />
+        <div className="px-4 sm:px-6 lg:px-8">{children}</div>
       </div>
+      <Toaster />
     </div>
-  )
+  );
 }
