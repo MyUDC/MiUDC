cf38a2284d65931a82fd857104805c3d6839fb6c
Luis Pinto <lpinto1@ucol.mx>
2024-09-19 12:41:32 -0600

    chore: implement slide per view on careerCategories

--- src/app/(auth)/(bottom-nav)/career-catalog/page.tsx
+++ src/app/(auth)/(bottom-nav)/career-catalog/page.tsx
@@ -14,13 +14,13 @@ export default function CareerCatalogPage() {
   return (
     <>
       <AppBar />
-      <div className="flex justify-center pt-4">
+      <div className="flex justify-center py-4">
         {/** Carreras favoritas */}
         <div className="w-full sm:max-w-5xl mt-16 px-4">
           <section>
             <div className="flex justify-between items-center">
               <p className="max-w-2xl mt-1 text-gray-500 text-sm uppercase">
-                Carreras favoritas de la comunidad
+                Carreras con más interacciones
               </p>
             </div>
             <div className="flex justify-between items-center mb-4">
@@ -45,9 +45,6 @@ export default function CareerCatalogPage() {
               <h1 className="max-w-2xl text-3xl font-bold text-black tracking-tight leading-none">
                 Explorar Categorías
               </h1>
-              <Button variant="ghost" className="text-md text-green">
-                Ver más
-              </Button>
             </div>
             <CareerCategories />
           </section>
