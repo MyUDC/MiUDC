bb41b4194964df67b87b015d68a77bebef8635a9
Luis Pinto <lpinto1@ucol.mx>
2024-09-25 14:24:48 -0600

    chore: delete ImagesLayout folder and implement Swiper on PostImages.tsx

--- src/shared/components/Testimony/ImagesLayout/index.ts
+++
@@ -1,5 +0,0 @@
-import SingleImages from "./SingleImages";
-import DoubleImages from "./DoubleImages";
-import ManyImages from "./ManyImages";
-
-export { SingleImages, DoubleImages, ManyImages };
--- src/shared/components/Testimony/PostImages.tsx
+++ src/shared/components/Testimony/PostImages.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ImageView from "../ImageView";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 interface Props {
   imageUrls?: string[];
@@ -9,6 +11,11 @@ interface Props {
 export const PostImages = ({ imageUrls }: Props) => {
   const [showImageView, setShowImageView] = useState(false);
   const [imageIndex, setImageIndex] = useState(0);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setIsLoaded(true);
+  }, []);
 
   if (!imageUrls) return null;
 
@@ -23,14 +30,23 @@ export const PostImages = ({ imageUrls }: Props) => {
 
   return (
     <>
-      <div className=" ml-[4.3rem]  relative flex overflow-x-auto no-scrollbar">
-        {imageUrls.map((src, index) => (
-          <PostImage
-            key={index}
-            src={src}
-            handleClick={() => handleImageOpen(index)}
-          />
-        ))}
+      <div className="ml-[4.3rem] relative select-none">
+        {isLoaded && (
+          <Swiper
+            slidesPerView={2.5}
+            spaceBetween={16}
+            className="mySwiper cursor-pointer pb-9"
+          >
+            {imageUrls.map((src, index) => (
+              <SwiperSlide key={index}>
+                <PostImage
+                  src={src}
+                  handleClick={() => handleImageOpen(index)}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        )}
       </div>
       {showImageView && (
         <ImageView
@@ -50,13 +66,16 @@ interface PostImageProps {
 
 const PostImage = ({ src, handleClick }: PostImageProps) => {
   return (
-    <div className="flex-none h-36 relative min-w-36 mr-4">
+    <div
+      className="aspect-square relative w-full h-36 select-none"
+      onClick={handleClick}
+    >
       <Image
         src={src}
         alt="Image"
         fill
         className="rounded-md border object-cover"
-        onClick={handleClick}
+        draggable="false"
       />
     </div>
   );
