ff61a73d502cf3ccb3c89bc1bb2dfc4e397bee9b
Luis Pinto <lpinto1@ucol.mx>
2024-10-08 16:07:52 -0600

    chore: implement BackButton to router.back() instead of /home route

--- src/shared/components/BackButton.tsx
+++ src/shared/components/BackButton.tsx
@@ -2,16 +2,22 @@
 
 import { Button } from "@/components/ui/button";
 import { IoArrowBack } from "react-icons/io5";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 export default function BackButton() {
+  const router = useRouter();
+
+  const handleBack = () => {
+    router.back();
+  };
+
   return (
-    <Link href="/home">
-      <Button
-        variant="outline"
-        className="fixed top-4 left-4 flex items-center z-20"
-      >
-        <IoArrowBack className="text-green h-4 w-4" />
-      </Button>
-    </Link>
+    <Button
+      variant="outline"
+      className="fixed top-4 left-4 flex items-center z-20"
+      onClick={handleBack}
+    >
+      <IoArrowBack className="text-green h-4 w-4" />
+    </Button>
   );
 }
