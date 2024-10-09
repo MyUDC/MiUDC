57fa6d6978d37df9453e5dc93096ea021f47f790
Luis Pinto <lpinto1@ucol.mx>
2024-10-09 10:08:43 -0600

    chore: resolve conflicts

--- src/shared/components/BackButton.tsx
+++ src/shared/components/BackButton.tsx
@@ -4,12 +4,29 @@ import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 import { IoArrowBack } from "react-icons/io5";
 
-export default function BackButton() {
+interface Props {
+  /**
+   * @param path 
+   * path to navigate to when the back button is clicked. 
+   * If not provided, the back button will navigate to the previous page.
+   * 
+   * @example
+   * path="/home"
+   * path="/career/telematica"
+   * 
+   * @default undefined
+   */
+  path?: string;
+}
+
+export default function BackButton({ path }: Props) {
   const router = useRouter();
 
   const handleBack = () => {
-    router.back();
-  };
+    if (path) router.push(path);
+    else router.back();
+  }
+
   return (
     <Button
       variant="outline"
