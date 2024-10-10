0055971175f0f688e9fa67e5be7ef8f3f196c68d
Luis Pinto <lpinto1@ucol.mx>
2024-10-10 11:51:41 -0600

    chore: add user career on UserMenuSheet

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,23 +1,23 @@
 import { $Enums } from "@prisma/client";
-import NextAuth,{DefaultSession} from "next-auth";
-import {JWT} from "next-auth/jwt";
+import NextAuth, { DefaultSession } from "next-auth";
+import { JWT } from "next-auth/jwt";
+import { Career } from "@prisma/client";
 
 interface IUser extends User {
   id: string;
   role: $Enums["Role"];
   username: string;
+  career?: Career;
 }
 
 declare module "next-auth" {
-  interface User extends IUser { }
+  interface User extends IUser {}
 
   interface Session {
     user?: User;
   }
 }
 
-
 declare module "next-auth/jwt" {
-  interface JWT extends IUser { }
+  interface JWT extends IUser {}
 }
-
